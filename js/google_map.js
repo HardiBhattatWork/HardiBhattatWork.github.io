a082e8fff336f85de640300ca2bd1d38c7c0654e
Hardi Bhatt <Dushthabuddhi@protonmail.com>
2026-02-19 11:13:01 -0500

    Update Google Maps API URL to use HTTPS for security

--- js/google_map.js
+++ js/google_map.js
@@ -1,4 +1,14 @@
-// Updated Google Maps API call to use HTTPS
-const apiKey = 'YOUR_API_KEY';
-const mapUrl = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
-// Rest of the code...
+// Google Maps API URL
+const GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY';
+
+// Initialize map function
+function initMap() {
+    // Map options
+    const options = {
+        zoom: 10,
+        center: { lat: 40.7128, lng: -74.0060 }
+    };
+
+    // New map
+    const map = new google.maps.Map(document.getElementById('map'), options);
+}
